04aaf8ee60b7d3f4f70e5053637d16a76fbfb7c5
Charan donthula <charandonthula995@gmail.com>
2025-08-13 21:42:46 +0530

    Added Day 17 - Sum of Three Integers

---
+++ day17.c
@@ -0,0 +1,21 @@
+// Problem Name: Sum of Three Numbers
+// Source: HackerRank
+// Link: https://www.hackerrank.com/challenges/cpp-input-and-output
+// Note: Reads three integers from input, calculates their sum, and outputs the result.
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    int a,b,c;
+    cin>>a>>b>>c;
+    cout<<(a+b+c)<<endl;
+    // s=a+b+c;
+    // cout<<s<<endl;  
+    return 0;
+}
