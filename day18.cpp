c6c3aeaec22048bde49f1ffebf9bce66bdb5fa6c
Charan donthula <charandonthula995@gmail.com>
2025-08-14 22:16:43 +0530

    Added Day 18

--- day18.cpp
+++ day18.cpp
@@ -16,3 +16,25 @@ int main() {
     printf("%d\n%ld\n%c\n%.3f\n%.9lf\n",a,b,c,d,e);
     return 0;
 }
+//Alternate way 
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+
+    cin >> a >> b >> c >> d >> e;
+
+    cout << a << "\n"
+         << b << "\n"
+         << c << "\n"
+         << fixed << setprecision(3) << d << "\n"
+         << fixed << setprecision(9) << e << "\n";
+
+    return 0;
+}
