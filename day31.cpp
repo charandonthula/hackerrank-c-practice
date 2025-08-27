f0def6dc215e8bf831582384589c568c7face82e
Charan donthula <charandonthula995@gmail.com>
2025-08-27 22:33:14 +0530

    Added Day 31 - Rectangle Area (Inheritance)

---
+++ day31.cpp
@@ -0,0 +1,55 @@
+//Problem: Rectangle Area (Inheritance)
+//Source: HackerRank
+//Link: https://www.hackerrank.com/challenges/rectangle-area/problem
+//Note: Demonstrates method overriding in C++. The child class (RectangleArea) redefines display() to print the area. `r_area.Rectangle::display();` is used to explicitly call the parent class display() so width and height are printed as well.
+#include <iostream>
+using namespace std;
+/*
+ * Create classes Rectangle and RectangleArea
+ */
+class Rectangle {
+protected:  
+    int width;
+    int height;
+public:
+    void display() {
+        cout << width << " " << height << endl;
+    }
+};
+
+// Derived class
+class RectangleArea : public Rectangle {
+public:
+    void read_input() {
+        cin >> width >> height;
+    }
+
+    void display() {
+        cout << width * height << endl;
+    }
+};
+
+int main()
+{
+    /*
+     * Declare a RectangleArea object
+     */
+    RectangleArea r_area;
+    
+    /*
+     * Read the width and height
+     */
+    r_area.read_input();
+    
+    /*
+     * Print the width and height
+     */
+    r_area.Rectangle::display();
+    
+    /*
+     * Print the area
+     */
+    r_area.display();
+    
+    return 0;
+}
